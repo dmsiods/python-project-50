c3cfa773f7e14e3158a31851c780371b50c6b828
dima <dmsiods0@gmail.com>
2024-01-23 13:46:56 +0300

    feat: step 6

--- tests/scripts/test_gendiff.py
+++ tests/scripts/test_gendiff.py
@@ -7,6 +7,7 @@ import pytest
     [
         ('tests/fixtures/file11.json', 'tests/fixtures/file12.json', 'tests/fixtures/result1.txt'),
         ('tests/fixtures/file11.yml', 'tests/fixtures/file12.yaml', 'tests/fixtures/result1.txt'),
+        ('tests/fixtures/file21.json', 'tests/fixtures/file22.json', 'tests/fixtures/result2.txt'),
     ]
 )
 def test_generate_diff(file1, file2, file_expected):
