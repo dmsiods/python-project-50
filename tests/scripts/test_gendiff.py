a187fb5bfe1b59af63f69cda777f21a96682defd
dima <dmsiods0@gmail.com>
2024-01-25 10:56:50 +0300

    feat: step 8 (refactoring)

--- tests/scripts/test_gendiff.py
+++ tests/scripts/test_gendiff.py
@@ -5,14 +5,21 @@ import pytest
 @pytest.mark.parametrize(
     "file1,file2,file_expected,output_format",
     [
-        ('tests/fixtures/file11.json', 'tests/fixtures/file12.json', 'tests/fixtures/result1.txt', 'stylish'),
-        ('tests/fixtures/file11.yml', 'tests/fixtures/file12.yaml', 'tests/fixtures/result1.txt', 'stylish'),
-        ('tests/fixtures/file21.json', 'tests/fixtures/file22.json', 'tests/fixtures/result2.txt', 'stylish'),
-        ('tests/fixtures/file21.json', 'tests/fixtures/file22.json', 'tests/fixtures/result2_plain.txt', 'plain'),
+        ('tests/fixtures/file11.json', 'tests/fixtures/file12.json',
+         'tests/fixtures/result1.txt', 'stylish'),
+        ('tests/fixtures/file11.yml', 'tests/fixtures/file12.yaml',
+         'tests/fixtures/result1.txt', 'stylish'),
+        ('tests/fixtures/file21.json', 'tests/fixtures/file22.json',
+         'tests/fixtures/result2.txt', 'stylish'),
+        ('tests/fixtures/file21.json', 'tests/fixtures/file22.json',
+         'tests/fixtures/result2_plain.txt', 'plain'),
+        ('tests/fixtures/file21.json', 'tests/fixtures/file22.json',
+         'tests/fixtures/result2_json.txt', 'json'),
     ]
 )
 def test_generate_diff(file1, file2, file_expected, output_format):
     with open(file_expected) as file:
         expected = file.read()
 
-    assert generate_diff(file1, file2, output_format).strip() == expected.strip()
+    assert (generate_diff(file1, file2, output_format).strip()
+            == expected.strip())
